2a3d2089af2af15a29a798fb9c3bbe61f4252759
Charles Ivia <iviacharles@gmail.com>
2020-06-30 00:56:51 +0300

    Created typing functionality

--- typing-effect/script.js
+++ typing-effect/script.js
@@ -8,3 +8,21 @@ const erasingDelay = 90;
 const newTextDelay = 2000;
 let textArrayIndex = 0;
 let charIndex = 0;
+
+function type() {
+  if (!cursorSpan.classList.contains("typing")) {
+    cursorSpan.classList.add("typing");
+  }
+
+  //Type untyped letter
+
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+  }
+}
+
+type();
