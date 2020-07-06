d68cc40875ca3b1f64e2b1e3f5a53989437ebe91
Charles Ivia <iviacharles@gmail.com>
2020-07-06 18:28:48 +0300

    Fetch dom elements and set variables

---
+++ modal-component/script.js
@@ -0,0 +1,4 @@
+//Fetch dom elements and set variables
+const openBtn = document.getElementById("open");
+const closeBtn = document.getElementById("close");
+const modal = document.getElementsByClassName("modal-container");
