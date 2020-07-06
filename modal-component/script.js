544b7d6a95d2d8856e11c571e0fc25c0da561702
Charles Ivia <iviacharles@gmail.com>
2020-07-06 18:40:27 +0300

    Modal open and close logic

--- modal-component/script.js
+++ modal-component/script.js
@@ -1,4 +1,16 @@
 //Fetch dom elements and set variables
 const openBtn = document.getElementById("open");
 const closeBtn = document.getElementById("close");
-const modal = document.getElementsByClassName("modal-container");
+const modal = document.querySelector(".modal-container");
+
+//Toggle
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+  openBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  openBtn.style.display = "flex";
+});
